95169b6bab50af04156a28739b12d73544042b75
Unknown <1801180@s.asojuku.ac.jp>
2020-11-06 14:12:47 +0900

    作成：貯金額修正画面

---
+++ app/src/main/java/com/example/moneygement/UpdateSavingsAmountActivity.kt
@@ -0,0 +1,11 @@
+package com.example.moneygement
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class UpdateSavingsAmountActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_update_savings_amount)
+    }
+}
