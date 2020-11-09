08638cd0a4cf3df914f24d68a08983f8ef26dfe6
hoashi <akane.hoashi@outlook.jp>
2020-11-09 15:00:20 +0900

    追加：ledgerのgraphqlスキーマ, リポジトリファイル

---
+++ app/src/main/java/com/example/moneygement/repository/Ledger.kt
@@ -0,0 +1,56 @@
+package com.example.moneygement.repository
+
+import com.apollographql.apollo.coroutines.await
+import com.apollographql.apollo.exception.ApolloException
+import com.example.LedgerQuery
+import com.example.LedgersQuery
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
+
+class Ledger: GraphqlBase() {
+
+    private val apolloClient = super.access()
+
+    suspend fun getLedgerList(userId: Int): List<LedgersQuery.Ledger1>? {
+        var ledgerIdList: List<LedgersQuery.Ledger1>? = null
+
+        val job = GlobalScope.launch {
+            var response = try{
+                apolloClient.query(LedgersQuery(1)).await()
+            }catch(e: ApolloException){
+                e.printStackTrace()
+                return@launch
+            }
+
+            var result = response.data?.ledger()
+            if(result == null || response.hasErrors()){
+                return@launch
+            }else{
+                ledgerIdList = result.ledgers()
+            }
+        }
+        job.join()
+        return ledgerIdList
+    }
+
+    suspend fun getLedger(id: Int): LedgerQuery.Ledger1?{
+        var ledger: LedgerQuery.Ledger1? = null
+
+        val job = GlobalScope.launch {
+            var response = try{
+                apolloClient.query(LedgerQuery(1)).await()
+            }catch(e: ApolloException){
+                e.printStackTrace()
+                return@launch
+            }
+
+            val result = response.data?.ledger()
+            if(result == null || response.hasErrors()){
+                return@launch
+            }else{
+                ledger = result.ledger()
+            }
+        }
+        return ledger
+    }
+}
