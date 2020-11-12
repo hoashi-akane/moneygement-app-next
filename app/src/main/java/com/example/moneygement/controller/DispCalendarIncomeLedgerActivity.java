1d6faec489468ec506aca786840e08f928d1fb0d
hoashi <akane.hoashi@outlook.jp>
2020-11-13 01:02:27 +0900

    変更：各種名前

--- app/src/main/java/com/example/moneygement/controller/DispCalendarIncomeLedgerActivity.java
+++ app/src/main/java/com/example/moneygement/controller/DispCalendarIncomeLedgerActivity.java
@@ -8,43 +8,37 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CalendarView;
-
 import com.example.moneygement.R;
 
-public class DispCalenderLedgerActivity extends AppCompatActivity {
+public class DispCalendarIncomeLedgerActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_disp_calender_household_account_book);
+        setContentView(R.layout.activity_disp_calendar_income_ledger);
         //CalenderViewにリスナーを設定
         ((CalendarView) findViewById(R.id.calendarview)).setOnDateChangeListener(listener);
     }
+    
 
-    //カレンダーの日付部分タップ時のリスナー
     CalendarView.OnDateChangeListener listener = new CalendarView.OnDateChangeListener() {
-        //view・・・押下されたカレンダーのインスタンス
-        //year・・・タップされた日付の「年」
-        //month・・・タップされた日付の「月」※月は０月から始まるから＋１する
-        //dayOfMonth・・・タップされた日付の「日」
         @Override
-        public void onSelectedDayChange(@NonNull CalendarView view, int year, int month, int dayOfMonth){
-            Integer expenseYear = year;
-            Integer expenseMonth = month+1;
-            Integer expenseDay = dayOfMonth;
+        public void onSelectedDayChange(@NonNull CalendarView view, int year, int month, int dayOfMonth) {
+            Integer incomeYear = year;
+            Integer incomeMonth = month+1;
+            Integer incomeDay = dayOfMonth;
 
             //インテント生成
             Intent intent = new Intent
-                    (DispCalenderLedgerActivity.this,InputIncomeHouseholdAccountBookActivity.class);
+                    (DispCalendarIncomeLedgerActivity.this, InputIncomeHouseholdAccountBookActivity.class);
             //年月日を受け渡す
-            intent.putExtra("expenseYear",expenseYear);
-            intent.putExtra("expenseMonth",expenseMonth);
-            intent.putExtra("expenseDay",expenseDay);
+            intent.putExtra("incomeYear",incomeYear);
+            intent.putExtra("incomeMonth",incomeMonth);
+            intent.putExtra("incomeDay",incomeDay);
 
             startActivity(intent);
         }
     };
-
     @Override
     protected void onResume() {
         super.onResume();
@@ -52,10 +46,9 @@ public class DispCalenderLedgerActivity extends AppCompatActivity {
         topButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(DispCalenderLedgerActivity.this, MainActivity.class);
+                Intent intent = new Intent(DispCalendarIncomeLedgerActivity.this,MainActivity.class);
                 startActivity(intent);
             }
         });
     }
-
 }
