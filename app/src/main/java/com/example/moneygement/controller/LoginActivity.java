3a928386f1c0d2fbcf1eabbf1422264906f8f201
ogawa-mayuka <1801188oga@gmail.com>
2020-11-13 12:38:42 +0900

    追加：ログイン画面

---
+++ app/src/main/java/com/example/moneygement/controller/LoginActivity.java
@@ -0,0 +1,23 @@
+package com.example.moneygement.controller;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.example.moneygement.DispCalendarIncomeHouseholdAccountBookActivity;
+import com.example.moneygement.R;
+import com.google.android.material.textfield.TextInputEditText;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+    }
+}
