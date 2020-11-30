55e0e97176424b24ea91ce103e6cf42072ee55b6
ogawa-mayuka <1801188oga@gmail.com>
2020-11-30 15:03:28 +0900

    追加：共有家計簿収入修正画面

---
+++ app/src/main/java/com/example/moneygement/controller/UpdateShareIncomeActivity.java
@@ -0,0 +1,14 @@
+package com.example.moneygement.controller;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import com.example.moneygement.*;
+public class UpdateShareIncomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_update_share_income);
+    }
+}
