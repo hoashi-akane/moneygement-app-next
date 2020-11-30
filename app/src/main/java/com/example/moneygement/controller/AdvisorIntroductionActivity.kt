99be86d14863d05df39a3a8cec8982526fd94de5
ogawa-mayuka <1801188oga@gmail.com>
2020-11-30 13:53:11 +0900

    作成：アドバイザ機能紹介

---
+++ app/src/main/java/com/example/moneygement/controller/AdvisorIntroductionActivity.kt
@@ -0,0 +1,19 @@
+package com.example.moneygement.controller
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.moneygement.R
+import kotlinx.android.synthetic.main.activity_advisor_introduction.*
+import androidx.fragment.app.Fragment as Fragment1
+
+class AdvisorIntroductionActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_advisor_introduction)
+
+    }
+
+
+
+}
