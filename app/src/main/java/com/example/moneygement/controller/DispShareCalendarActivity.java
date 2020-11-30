d4d619f70979dcc4f5849b7be72b4c6ab3986f8f
ogawa-mayuka <1801188oga@gmail.com>
2020-11-30 14:49:08 +0900

    追加：共有家計簿支出カレンダー入力画面

---
+++ app/src/main/java/com/example/moneygement/controller/DispShareCalendarActivity.java
@@ -0,0 +1,15 @@
+package com.example.moneygement.controller;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import com.example.moneygement.*;
+
+public class DispShareCalendarActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_disp_share_calendar);
+    }
+}
