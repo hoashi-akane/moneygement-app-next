1f242c2aeeeeab7f5a7f33286fed394f3350bd4c
hoashi <akane.hoashi@outlook.jp>
2020-12-02 00:29:50 +0900

    変更：登録処理、intent周り

--- app/src/main/java/com/example/moneygement/controller/InputExpenseHouseholdAccountBookActivity.kt
+++ app/src/main/java/com/example/moneygement/controller/InputExpenseHouseholdAccountBookActivity.kt
@@ -8,63 +8,91 @@ import android.widget.EditText
 import android.widget.Spinner
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import com.example.CreateExpenseDetailMutation
+import com.example.LedgerQuery
+import com.example.LedgersQuery
 import com.example.moneygement.R
 import com.example.moneygement.controller.InputExpenseHouseholdAccountBookActivity
+import com.example.moneygement.repository.Ledger
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
 
 class InputExpenseHouseholdAccountBookActivity : AppCompatActivity() {
-    private var expenseYear: String? = null
-    private var expenseMonth: String? = null
-    private var expenseDay: String? = null
-    private var expenseDate: String? = null
+    private lateinit var expenseYear: String
+    private lateinit var expenseMonth: String
+    private lateinit var expenseDay: String
+    private lateinit var expenseDate: String
+    private lateinit var insertDate: String
+    private var ledgerId: Int = 0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_input_household_account_book)
-        val date = findViewById<View>(R.id.date) as TextView
+        val dateTextView = findViewById<View>(R.id.date) as TextView
         //Intentのオブジェクトを生成する
         val rIntent = intent
+
+        ledgerId = rIntent.getIntExtra("ledgerId", 0)
         //年月日を受け取る
-        expenseYear = rIntent.getIntExtra("expenseYear", 0).toString()
-        expenseMonth = rIntent.getIntExtra("expenseMonth", 0).toString()
-        expenseDay = rIntent.getIntExtra("expenseDay", 0).toString()
-        expenseDate = expenseYear + "年" + expenseMonth + "月" + expenseDay + "日"
-        date.text = expenseDate
+        expenseDate = rIntent.getStringExtra("checkDay")
+        dateTextView.text = expenseDate
+        insertDate = expenseDate.replace("[年月]".toRegex(), "-")
+        insertDate = insertDate.replace("日","")
     }
 
     override fun onResume() {
         super.onResume()
+
         val continueButton = findViewById<View>(R.id.tocontinue) as Button
         val saveBtn = findViewById<View>(R.id.savebutton) as Button
         val cancelBtn = findViewById<View>(R.id.cancel) as Button
-        continueButton.setOnClickListener { //エディットテキストを探す
-            val Amountofmoney = findViewById<View>(R.id.editTextNumberSigned) as EditText
-            val textPersonName = findViewById<View>(R.id.editTextTextPersonName) as EditText
-            val textcategory = findViewById<View>(R.id.spinnerCategory) as Spinner
-            val txtmoney = Amountofmoney.text.toString()
-            val personName = textPersonName.text.toString()
-            val category = textcategory.selectedItem.toString()
+        val amounToMoney = findViewById<View>(R.id.editTextNumberSigned) as EditText
+        val textPersonName = findViewById<View>(R.id.editTextTextPersonName) as EditText
+        val textCategory = findViewById<View>(R.id.spinner) as Spinner
 
-//              保存処理
-            val intent = Intent(this@InputExpenseHouseholdAccountBookActivity, InputExpenseHouseholdAccountBookActivity::class.java)
-            val money = "txtmoney".toInt()
-            intent.putExtra("money", money)
-            intent.putExtra("personName", personName)
-            intent.putExtra("cotegory", category)
-            startActivity(intent)
-        }
-        saveBtn.setOnClickListener {
-            val amounToMoney = findViewById<View>(R.id.editTextNumberSigned) as EditText
-            val textPersonName = findViewById<View>(R.id.editTextTextPersonName) as EditText
-            val textCategory = findViewById<View>(R.id.spinnerCategory) as Spinner
-            val txtmoney = amounToMoney.text.toString()
-            val personName = textPersonName.text.toString()
+        continueButton.setOnClickListener { //エディットテキストを探す
+            val money = amounToMoney.text.toString().toInt()
+            val note = textPersonName.text.toString()
             val category = textCategory.selectedItem.toString()
 
 //              保存処理
+            val createExpenseDetailMutation = CreateExpenseDetailMutation.builder()
+                    .ledgerId(ledgerId)
+                    .categoryId(1)
+                    .date(insertDate)
+                    .amount(money)
+                    .note(note)
+                    .build()
+
+            Ledger().insertExpense(createExpenseDetailMutation)
+
+            val intent = Intent(this@InputExpenseHouseholdAccountBookActivity, InputExpenseHouseholdAccountBookActivity::class.java)
+            intent.putExtra("checkDay", expenseDate)
+            intent.putExtra("ledgerId", ledgerId)
+            startActivity(intent)
+        }
+        saveBtn.setOnClickListener {
+            val money = amounToMoney.text.toString().toInt()
+            val note = textPersonName.text.toString()
+            val category = textCategory.selectedItem.toString()
+
+            //              保存処理
+            val createExpenseDetailMutation = CreateExpenseDetailMutation.builder()
+                    .ledgerId(ledgerId)
+                    .categoryId(1)
+                    .date(insertDate)
+                    .amount(money)
+                    .note(note)
+                    .build()
+
+            Ledger().insertExpense(createExpenseDetailMutation)
+
             val intent = Intent(this@InputExpenseHouseholdAccountBookActivity, DispLedgerActivity::class.java)
             startActivity(intent)
         }
         cancelBtn.setOnClickListener {
             val intent = Intent(this@InputExpenseHouseholdAccountBookActivity, DispCalenderExpensesLedgerActivity::class.java)
+            intent.putExtra("ledgerId", ledgerId)
             startActivity(intent)
         }
     }
--- app/src/main/java/com/example/moneygement/controller/InputIncomeHouseholdAccountBookActivity.kt
+++ app/src/main/java/com/example/moneygement/controller/InputIncomeHouseholdAccountBookActivity.kt
@@ -8,26 +8,37 @@ import android.widget.EditText
 import android.widget.Spinner
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import com.example.CreateIncomeDetailMutation
+import com.example.LedgerQuery
 import com.example.moneygement.R
 import com.example.moneygement.controller.InputIncomeHouseholdAccountBookActivity
+import com.example.moneygement.repository.Ledger
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
+import java.util.*
 
 class InputIncomeHouseholdAccountBookActivity : AppCompatActivity() {
-    private var incomeYear: String? = null
-    private var incomeMonth: String? = null
-    private var incomeDay: String? = null
-    private var incomeDate: String? = null
+    private lateinit var incomeYear: String
+    private lateinit var incomeMonth: String
+    private lateinit var incomeDay: String
+    private lateinit var incomeDate: String
+    private lateinit var insertDate: String
+    private var ledgerId: Int = 0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_input_income_household_account_book)
-        val date = findViewById<View>(R.id.date) as TextView
+        val dateTextView = findViewById<View>(R.id.date) as TextView
         //Intentのオブジェクトを生成する
         val rIntent = intent
+
+        ledgerId = rIntent.getIntExtra("ledgerId", 0)
         //年月日を受け取る
-        incomeYear = rIntent.getIntExtra("incomeYear", 0).toString()
-        incomeMonth = rIntent.getIntExtra("incomeMonth", 0).toString()
-        incomeDay = rIntent.getIntExtra("incomeDay", 0).toString()
-        incomeDate = incomeYear + "年" + incomeMonth + "月" + incomeDay + "日"
-        date.text = incomeDate
+        incomeDate = rIntent.getStringExtra("checkDay")
+        println(incomeDate)
+        dateTextView.text = incomeDate
+        insertDate = incomeDate.replace("[年月]".toRegex(), "-")
+        insertDate = insertDate.replace("日","")
     }
 
     override fun onResume() {
@@ -36,37 +47,57 @@ class InputIncomeHouseholdAccountBookActivity : AppCompatActivity() {
         val continueButton = findViewById<View>(R.id.tocontinue) as Button
         val saveBtn = findViewById<View>(R.id.savebutton) as Button
         val cancelBtn = findViewById<View>(R.id.cancel) as Button
-        continueButton.setOnClickListener { //              値を受取り
-            val amounToMoney = findViewById<View>(R.id.editTextNumberSigned) as EditText
-            val textPersonName = findViewById<View>(R.id.editTextTextPersonName) as EditText
-            val textCategory = findViewById<View>(R.id.spinnerCategory) as Spinner
-            val txtmoney = amounToMoney.text.toString()
-            val personName = textPersonName.text.toString()
+        val amounToMoney = findViewById<View>(R.id.editTextNumberSigned) as EditText
+        val textPersonName = findViewById<View>(R.id.editTextTextPersonName) as EditText
+        val textCategory = findViewById<View>(R.id.spinnerCategory) as Spinner
+
+        continueButton.setOnClickListener {
+            //              値を受取り
+            val money = (amounToMoney.text.toString()).toInt()
+            val note = textPersonName.text.toString()
             val category = textCategory.selectedItem.toString()
 
 //              保存処理
+            val createIncomeDetailMutation = CreateIncomeDetailMutation.builder()
+                    .ledgerId(ledgerId)
+                    .categoryId(1)
+                    .date(insertDate)
+                    .amount(money)
+                    .note(note)
+                    .build()
+
+            Ledger().insertIncome(createIncomeDetailMutation)
+//                    .
+
             val intent = Intent(this@InputIncomeHouseholdAccountBookActivity, InputIncomeHouseholdAccountBookActivity::class.java)
             //              同じ日付をセット（続けて入力する場合日付が必要
-            val money = "txtmoney".toInt()
-            intent.putExtra("incomeYear", incomeYear)
-            intent.putExtra("incomeMonth", incomeMonth)
-            intent.putExtra("incomeDay", incomeDay)
+            intent.putExtra("checkDay", incomeDate)
+            intent.putExtra("ledgerId", ledgerId)
             startActivity(intent)
         }
+
         saveBtn.setOnClickListener {
-            val amounToMoney = findViewById<View>(R.id.editTextNumberSigned) as EditText
-            val textPersonName = findViewById<View>(R.id.editTextTextPersonName) as EditText
-            val textCategory = findViewById<View>(R.id.spinnerCategory) as Spinner
-            val txtmoney = amounToMoney.text.toString()
-            val personName = textPersonName.text.toString()
+            val money = (amounToMoney.text.toString()).toInt()
+            val note = textPersonName.text.toString()
             val category = textCategory.selectedItem.toString()
 
 //              保存処理
+            val createIncomeDetailMutation = CreateIncomeDetailMutation.builder()
+                    .ledgerId(ledgerId)
+                    .categoryId(1)
+                    .date(insertDate)
+                    .amount(money)
+                    .note(note)
+                    .build()
+
+            Ledger().insertIncome(createIncomeDetailMutation)
+
             val intent = Intent(this@InputIncomeHouseholdAccountBookActivity, DispLedgerActivity::class.java)
             startActivity(intent)
         }
         cancelBtn.setOnClickListener {
             val intent = Intent(this@InputIncomeHouseholdAccountBookActivity, DispCalendarIncomeLedgerActivity::class.java)
+            intent.putExtra("ledgerId", ledgerId)
             startActivity(intent)
         }
     }
