575ff3da8e1b86c948729ab1adb98e010103b51c
hoashi <akane.hoashi@outlook.jp>
2020-12-04 16:30:26 +0900

    追加：共有家計簿一覧取得処理

---
+++ app/src/main/java/com/example/moneygement/repository/ShareRepository.kt
@@ -0,0 +1,33 @@
+package com.example.moneygement.repository
+
+import com.apollographql.apollo.coroutines.await
+import com.apollographql.apollo.exception.ApolloException
+import com.example.ShareLedgersQuery
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
+
+class ShareRepository: GraphqlBase(){
+    private val apolloClient = super.access()
+
+    suspend fun getShareLedgerList(userId: Int): List<ShareLedgersQuery.ShareLedger>? {
+        var ledgerIdList: List<ShareLedgersQuery.ShareLedger>? = null
+
+        val job = GlobalScope.launch {
+            var response = try{
+                apolloClient.query(ShareLedgersQuery(userId)).await()
+            }catch(e: ApolloException){
+                e.printStackTrace()
+                return@launch
+            }
+
+            var result = response.data?.ledger()
+            if(result == null || response.hasErrors()){
+                return@launch
+            }else{
+                ledgerIdList = result.shareLedgers()
+            }
+        }
+        job.join()
+        return ledgerIdList
+    }
+}
