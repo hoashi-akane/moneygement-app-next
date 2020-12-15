229814b20a436bad29aa63f01deb855592e67921
lllllllll <colombo044apd@outlook.jp>
2020-12-15 11:34:08 +0900

    変更：ユーザ一覧

--- app/src/main/java/com/example/moneygement/controller/UserRecyclerAdapter.kt
+++ app/src/main/java/com/example/moneygement/controller/UserRecyclerAdapter.kt
@@ -7,16 +7,18 @@ import androidx.recyclerview.widget.RecyclerView
 import com.example.InsertUserMutation
 import com.example.SavingsDetailsQuery
 import com.example.moneygement.R
+import kotlinx.android.synthetic.main.activity_disp_user_list_item.view.*
 import kotlinx.android.synthetic.main.savings_history_list_item.view.*
 
 class UserRecyclerAdapter {private var userList: List<InsertUserMutation.CreateUser>):
-       RecyclerView.Adapter<UserRecyclerAdapter.UserMyViewHolder>(){
+       RecyclerView.Adapter<UserRecyclerAdapter.UserMyViewHolder>()
+    {
 
         class UserMyViewHolder(val view: View) : RecyclerView.ViewHolder(view)
 
         override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerAdapter.MyViewHolder {
             val view = LayoutInflater.from(parent.context)
-                    .inflate(R.layout.savings_history_list_item, parent, false) as View
+                    .inflate(R.layout.activity_disp_user_list_item, parent, false) as View
 
             return UserMyViewHolder(view)
         }
@@ -24,9 +26,9 @@ class UserRecyclerAdapter {private var userList: List<InsertUserMutation.CreateU
         override fun getItemCount() = userList.size
 
         override fun onBindViewHolder(holder: RecyclerAdapter.MyViewHolder, position: Int) {
-            var date  = userList[position].saving_date().toString().substring(0, 10).split("-")
+            var user = userList[position].nickname().split("-")
 
-            holder.view.savingDate.text = date[0]+"年"+date[1]+"月"+date[2]+"日"
-            holder.view.savingAmount.text = "+" + userList[position].saving_amount().toString()
+            holder.view.username.text = user
         }
+    }
 }
