93d98debdd06823c4633bb23f9955e3dd1872744
HOASHI AKANE <akane.hoashi@outlook.jp>
2022-05-21 22:26:37 +0900

    コードを読みやすく整形

--- app/src/main/java/com/example/moneygement/service/AuthService.kt
+++ app/src/main/java/com/example/moneygement/service/AuthService.kt
@@ -7,49 +7,46 @@ import androidx.security.crypto.MasterKeys
 import com.example.LoginQuery
 import com.example.moneygement.model.User
 import com.example.moneygement.repository.UserRepository
-import kotlinx.coroutines.*
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.runBlocking
 import kotlin.coroutines.CoroutineContext
 
 class AuthService : CoroutineScope {
 
     private val job = Job()
-    fun createUser(){
-
-    }
 
     override val coroutineContext: CoroutineContext
         get() = Dispatchers.Main + job
 
-    fun getLoginQueryToUserModel(loginInfo: LoginQuery.Login): User{
-
-        val user = User()
-        user.id = loginInfo.id()
-        user.name = loginInfo.name()
-        user.mail = loginInfo.email()
-        user.nickName = loginInfo.nickname()
-
-        return user
-    }
+    fun getLoginQueryToUserModel(loginInfo: LoginQuery.Login): User =
+        User().also {
+            it.id = loginInfo.id()
+            it.name = loginInfo.name()
+            it.mail = loginInfo.email()
+            it.nickName = loginInfo.nickname()
+        }
 
     fun createAuthSharedPreferences(context: Context): SharedPreferences {
         val keyGenParameterSpec = MasterKeys.AES256_GCM_SPEC
         val masterKeyAlias = MasterKeys.getOrCreate(keyGenParameterSpec)
         return EncryptedSharedPreferences.create(
-                "user_info",
-                masterKeyAlias,
-                context,
-                EncryptedSharedPreferences.PrefKeyEncryptionScheme.AES256_SIV,
-                EncryptedSharedPreferences.PrefValueEncryptionScheme.AES256_GCM
+            "user_info",
+            masterKeyAlias,
+            context,
+            EncryptedSharedPreferences.PrefKeyEncryptionScheme.AES256_SIV,
+            EncryptedSharedPreferences.PrefValueEncryptionScheme.AES256_GCM
         )
     }
 
-    fun checkUserInfo(context: Context): LoginQuery.Login?{
+    fun checkUserInfo(context: Context): LoginQuery.Login? {
 
         val sharedPreferences = createAuthSharedPreferences(context)
         val email = sharedPreferences.getString("email", "")
         val password = sharedPreferences.getString("password", "")
 
-        if(email.equals("") || password.equals("")){
+        if (email.equals("") || password.equals("")) {
             return null
         }
         return runBlocking {
@@ -57,7 +54,7 @@ class AuthService : CoroutineScope {
         }
     }
 
-    fun logout(context: Context){
+    fun logout(context: Context) {
         val sharedPreferences = context.getSharedPreferences("user_info", 0)
         sharedPreferences.edit().clear().commit()
     }
